e35fa951e5ea0ff3322e4ac3b30a91a751a59a8f
mccormackj <mccormackj@utexas.edu>
2022-07-27 19:45:48 -0500

    Adds FontAwesome symbols for homepage

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,16 @@
 import React from 'react';
- 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+
 function Contact (){
  return <div>
+    <FontAwesomeIcon icon = {faLinkedin} />
+    <FontAwesomeIcon icon = {faGithubSquare} />
+    <FontAwesomeIcon icon = { faPhone } />
+    <FontAwesomeIcon icon = { faEnvelope } />
             <h2> Phone Number</h2>
             <h2> Email </h2>
             <address>
