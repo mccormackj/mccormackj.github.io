3c286c7a5df461ef3f3d8330508f2c7ebd1e0fc2
mccormackj <mccormackj@utexas.edu>
2022-07-28 13:30:50 -0500

    Changes font and style of menu items, and highlights current page

--- src/App.js
+++ src/App.js
@@ -7,34 +7,45 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
+  Link,
 } from 'react-router-dom';
 
 
-class App extends Component {
-  render() {
-    return (
-       <Router>
-        <div className="App">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About Us</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact Us</Link>
-            </li>
-          </ul>
-          <Routes>
-              <Route exact path='/' element={< Home />}></Route>
-              <Route exact path='/about' element={< About />}></Route>
-              <Route exact path='/contact' element={< Contact />}></Route>
-          </Routes>
-        </div>
-       </Router>
-   );
+function App () {
+  return (
+      <Router>
+      <div className="App">
+        <ul id ="Menu">
+          <li>
+            <Link to="/" onClick = {() => highlightActive("/")} >Home</Link>
+          </li>
+          <li>
+            <Link to="/about" onClick = {() => highlightActive("/about")} >About Me</Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick = {() => highlightActive("/contact")} >Contact Me</Link>
+          </li>
+        </ul>
+        <Routes>
+            <Route exact path='/' element = {<Home />} ></Route>
+            <Route exact path='/about' element={< About />}></Route>
+            <Route exact path='/contact' element={< Contact />}></Route>
+        </Routes>
+      </div>
+      </Router>
+  );
+}
+
+function highlightActive(path) {
+  var links = document.getElementById("Menu").getElementsByTagName("a")
+  for(var i = 0; i < links.length; i++) {
+    var linkPath = links[i].pathname
+    console.log(path + " " + linkPath)
+    if(path == linkPath){
+      links[i].classList.add("selected")
+    } else {
+      links[i].classList.remove("selected")
+    }
   }
 }
 
