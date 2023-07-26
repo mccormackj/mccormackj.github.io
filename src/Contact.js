72e4f6bf0526723a1f46197210cf184140285161
mccormackj <mccormackj@utexas.edu>
2023-07-26 15:39:03 -0500

    Sizes up logos and adds links

--- src/Contact.js
+++ src/Contact.js
@@ -7,10 +7,10 @@ import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 
 function Contact (){
  return <div>
-    <FontAwesomeIcon icon = {faLinkedin} />
-    <FontAwesomeIcon icon = {faGithubSquare} />
-    <FontAwesomeIcon icon = { faPhone } />
-    <FontAwesomeIcon icon = { faEnvelope } />
+    <FontAwesomeIcon class="contact-icon" icon = { faLinkedin} size="5x" onClick={() => openURL("https://www.linkedin.com/in/johanna-mccormack/")}/>
+    <FontAwesomeIcon class="contact-icon" icon = { faGithubSquare} size="5x" onClick={() => openURL("https://www.github.com/mccormackj")}/>
+    <FontAwesomeIcon class="contact-icon" icon = { faPhone } size="5x" onClick={() => openURL("tel:+2149237938")}/>
+    <FontAwesomeIcon class="contact-icon" icon = { faEnvelope } size="5x" onClick={() => openURL("mailto: mccormackj2001@gmail.com")}/>
             <h2> Phone Number</h2>
             <h2> Email </h2>
             <address>
@@ -22,4 +22,7 @@ function Contact (){
         </div>
 }
  
+function openURL(url){
+    window.open(url)
+}
 export default Contact;
