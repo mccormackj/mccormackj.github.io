3f121e39cfe350188a96def485c3377810ea0610
mccormackj <mccormackj@utexas.edu>
2023-08-08 14:04:30 -0500

    Style icons for Contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Contact.css';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
@@ -7,19 +7,15 @@ import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 
 function Contact (){
  return <div>
-    <FontAwesomeIcon class="contact-icon" icon = { faLinkedin} size="5x" onClick={() => openURL("https://www.linkedin.com/in/johanna-mccormack/")}/>
-    <FontAwesomeIcon class="contact-icon" icon = { faGithubSquare} size="5x" onClick={() => openURL("https://www.github.com/mccormackj")}/>
-    <FontAwesomeIcon class="contact-icon" icon = { faPhone } size="5x" onClick={() => openURL("tel:+2149237938")}/>
-    <FontAwesomeIcon class="contact-icon" icon = { faEnvelope } size="5x" onClick={() => openURL("mailto: mccormackj2001@gmail.com")}/>
-            <h2> Phone Number</h2>
-            <h2> Email </h2>
-            <address>
-                You can find us here:<br />
-                GeeksforGeeks<br />
-                5th & 6th Floor, Royal Kapsons, A- 118, <br />
-                Sector- 136, Noida, Uttar Pradesh (201305)
-            </address>
+    <h2> Looking to connect?</h2>
+        <p> Currently, I am based in Austin, TX. The best way to reach me is via email at mccormackj2001@gmail.com. You can also send me a connection request
+         on LinkedIn or view my GitHub account!</p>
+         <div class = "icons">
+        <FontAwesomeIcon class="contact-icon" icon = { faLinkedin} onClick={() => openURL("https://www.linkedin.com/in/johanna-mccormack/")}/>
+        <FontAwesomeIcon class="contact-icon" icon = { faGithubSquare} onClick={() => openURL("https://www.github.com/mccormackj")}/>
+        <FontAwesomeIcon class="contact-icon" icon = { faEnvelope } onClick={() => openURL("mailto: mccormackj2001@gmail.com")}/>
         </div>
+    </div>
 }
  
 function openURL(url){
